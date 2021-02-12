1a9166c69becc2c2327152cd235421e44151f4bf
Lucas Kranz <kranz12@gmail.com>
2021-02-12 16:13:36 -0300

    solução do exercício das letras duplicadas

---
+++ LetrasDuplicadas/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LetrasDuplicadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] duplicados1 = { "abracadabra", "allottee", "assessee" };
+            string[] duplicados2 = {"kelless", "keenness"};
+            string[] duplicados3 = { "booooooola", "macaarrrrrao", "carrrrrrneeeeee" };
+
+            Util.RemovaLetrasDuplicadas(ref duplicados1);
+            Util.RemovaLetrasDuplicadas(ref duplicados2);
+            Util.RemovaLetrasDuplicadas(ref duplicados3);
+        }
+    }
+}
---
+++ LetrasDuplicadas/Util.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LetrasDuplicadas
+{
+    class Util
+    {
+        public static void RemovaLetrasDuplicadas(ref string[] palavras)
+        {
+            int cont = 0;
+            char letraAtual, letraAnterior;
+
+            for (int i = 0; i < palavras.Length; i++)
+            {
+                int n = 0;
+                while (n < palavras[i].Length)
+                {
+                    if (n == 0)
+                    {
+                        n++;
+                        continue;
+                    }
+
+                    letraAtual = palavras[i][n];
+                    letraAnterior = palavras[i][n - 1];
+
+                    if (letraAtual == letraAnterior)
+                        palavras[i] = palavras[i].Remove(n, 1);
+                    else
+                        n++;
+                }
+            }
+
+            foreach(string palavra in palavras)
+            {
+                Console.Write(palavra + " ");
+            }
+            Console.Write("\n");
+        } 
+    }
+}
