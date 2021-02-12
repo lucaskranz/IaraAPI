14d234e929385681262c7d443078f052eb3de876
Lucas Kranz <kranz12@gmail.com>
2021-02-12 13:53:54 -0300

    Acrescentada pasta para colocar exercicíos no GIT

---
+++ LINQ/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = { 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 };
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
